bc42ca2f8ac7ded121acfc241720335ba864ae76
mayo <70703716+mayo516@users.noreply.github.com>
2022-07-29 16:38:56 +0900

    Add files via upload

---
+++ 인프런 게임만들기/coding/code/js/game.js
@@ -0,0 +1,47 @@
+/* 코드를 작성하세요 */
+const key = {
+    keyDown : {},
+    keyValue : {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        88: 'attack',
+    }
+}
+
+const renderGame = () => {
+    hero.keyMotion();
+    window.requestAnimationFrame(renderGame);
+}
+
+const windowEvent = () => {
+    window.addEventListener('keydown', e => {
+        key.keyDown[key.keyValue[e.which] ] = true ;
+        
+    })
+
+    window.addEventListener('keyup', e => {
+        key.keyDown[key.keyValue[e.which] ] = false ;
+        
+    })
+}
+
+const logImg = () => {
+    const preLoadImgSrc = ['../../lib/images/ninja_attack.png','../../lib/images/ninja_run.png',]
+    preLoadImgSrc.forEach(arr =>{
+        const img = new Image();
+        img.src= arr;
+    })
+}
+
+let hero;
+const init = () => {
+    hero =  new Hero('.hero');
+    logImg();
+    windowEvent();
+    renderGame();
+}
+
+window.onload = () => {
+    init();
+}
